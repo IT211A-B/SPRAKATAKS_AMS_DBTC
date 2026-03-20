48fcd4598219d6e4104a47bf022cc05c28c073f8
mabirikkkk <maverick.tajanlangit@dbtc-cebu.edu.ph>
2026-03-20 20:21:51 +0800

    Initial Commit

---
+++ FrontEnd/Frontend/Frontend/Program.cs
@@ -0,0 +1,23 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAntiforgery();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Account/Login");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
+app.Run();
