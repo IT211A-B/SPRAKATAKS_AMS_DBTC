76c694ed59dc5a3511a905c92bb857632604bd96
lawrence <jameslaurence.gulfan@dbtc-cebu.edu.ph>
2026-03-20 20:50:32 +0800

    Delete AMS directory

--- AMS/AMS/DTO/CourseDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AttendanceManagementSystem.DTOs
-{
-    public class CourseDTO
-    {
-        public string CourseName { get; set; }
-
-        public int TeacherId { get; set; }
-    }
-}
